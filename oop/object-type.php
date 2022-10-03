6f232693e64d431b7186e780cda09edc36d52e89
Putrirenan <putrirenan@gmail.com>
2022-10-03 23:54:37 +0700

    Video 4 OOP Dasar Object Type

---
+++ oop/object-type.php
@@ -0,0 +1,48 @@
+<?php
+
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit,
+    $harga;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga  = 0)    // Constructor merupakan sebuah method yang spesial atau method yang khusus yg ada dalam sebuah kelas
+  {                                                                                                           // Karna constructor adalah method yang otomatis dijalankan ketika sebuay kelas di instansiasi
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";   // fungsi dari $this-> untuk mengambil isi dari property yang ada didalam kelas yang bersangkutan ketika dibuat instancenya
+  }
+}
+
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    $str = "$produk->judul | {$produk->getLabel()} (Rp.{$produk->harga})";
+    return $str;
+  }
+}
+
+
+
+
+// method adalah function yang ada didalam class
+
+$produk1 = new Produk("Naruto", "Masasi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Drukmann", "Sony Computer", 250000);
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
