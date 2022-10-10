d1465f1f2a628d55b014688a2d7b4c933aab7945
Putrirenan <putrirenan@gmail.com>
2022-10-11 00:04:45 +0700

    Video MVC 7 Database Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_c_203040153/mvc/public');
+
+//  database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
--- mvc/app/core/Constants.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-
-define('BASEURL', 'http://localhost/prakweb_c_203040153/mvc/public');
