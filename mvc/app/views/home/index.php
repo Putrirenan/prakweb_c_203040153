6e9415fdab3ba59b926f39591dc834f1a2b74727
Putrirenan <putrirenan@gmail.com>
2022-10-10 22:46:00 +0700

    Video MVC 6 Models

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="jumbotron mt-5">
     <h1>Selamat Datang di Website Rere.</h1>
-    <p class="lead">Halo nama saya Renandra Rahadian Putri NPM 203040153 Kelas C.</p>
+    <p class="lead">Halo nama panggilan saya <?= $data['nama']; ?> dan nama lengkap saya Renandra Rahadian Putri NPM 203040153 Kelas C.</p>
     <hr class="my-4">
     <p>Saya adalah seorang mahasiswa Universitas Pasundan Semester 5. Untuk info lebih lanjut silahkan klik tombol dibawah ini.</p>
     <a class="btn btn-primary btn-lg" href="#" role="button">Lihat detail</a>
